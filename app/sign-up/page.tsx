366ef9b27fe21f3ec2efce00090656e3d90a5c8b
Aayush <aayush130405@gmail.com>
2025-09-07 19:47:50 +0530

    fix bug

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -21,6 +21,7 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
 function Signup() {
     const {isLoaded, signUp, setActive} = useSignUp()
+
     const [emailAddress, setEmailAddress] = useState('')
     const [password, setPassword] = useState('')
     const [pendingVerification, setPendingVerification] = useState(false)
@@ -77,9 +78,10 @@ function Signup() {
         } catch (error: any) {
             console.log(JSON.stringify(error, null, 2));
             setError(error.errors[0].message || "Something went wrong. Try again.")
-        }
+        } 
+    }
 
-        return (
+  return (
             <div className="flex items-center justify-center min-h-screen bg-background">
       <Card className="w-full max-w-md">
         <CardHeader>
@@ -168,8 +170,7 @@ function Signup() {
         </CardFooter>
       </Card>
     </div>
-        ) 
-    }
+  )
 }
 
 export default Signup
