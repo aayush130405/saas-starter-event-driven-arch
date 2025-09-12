26867a5b1de9b786c22dbe589d89359b88e66b94
Aayush <aayush130405@gmail.com>
2025-09-12 12:49:32 +0530

    update webhook api

--- app/api/webhook/register/route.ts
+++ app/api/webhook/register/route.ts
@@ -18,4 +18,39 @@ async function POST(req: Request) {
     if(!svix_id || !svix_timestamp || !svix_signature) {
         return new Response("Error occured - No svix headers")
     }
+
+    const payload = await req.json()
+    const body = JSON.stringify(payload)
+
+    const wh = new Webhook(WEBHOOK_SECRET);
+    
+    let evt: WebhookEvent
+
+    try {
+        evt = wh.verify(body, {
+            "svix-id": svix_id,
+            "svix-timestamp": svix_timestamp,
+            "svix-signature": svix_signature
+        }) as WebhookEvent
+    } catch (err) {
+        console.error("Error verifying webhook", err)
+        return new Response("Error occured", {status: 400})
+    }
+
+    const {id} = evt.data
+    const eventType = evt.type
+
+    if(eventType === "user.created") {
+        try {
+            const {email_addresses, primary_email_address_id} = evt.data
+
+            const primaryEmail = email_addresses.find((email) => email.id === primary_email_address_id)
+
+            if(!primaryEmail) {
+                return new Response("No primary email found", {status: 400})
+            }
+        } catch (error) {
+            
+        }
+    }
 }
