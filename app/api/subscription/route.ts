b4db8c021d4ae7eaabccf9eed3e3621036e08039
Aayush <aayush130405@gmail.com>
2025-09-13 12:24:14 +0530

    update subscription api

---
+++ app/api/subscription/route.ts
@@ -0,0 +1,74 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+export async function POST() {
+    const {userId} = await auth()
+
+    if(!userId) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    }
+
+    //add payment here
+
+    try {
+        const user = await prisma.user.findUnique({where: {id: userId}})
+
+        if(!user) {
+            return NextResponse.json({error: "User not found"}, {status: 404})
+        }
+
+        const subscriptionEnds = new Date()         //get the current date for subscription
+        subscriptionEnds.setMonth(subscriptionEnds.getMonth() + 1)      //extend the subscription for 1 month
+
+        const updatedUser = await prisma.user.update({where: {id: userId}, data: {isSubscribed: true, subscriptionEnds: subscriptionEnds}})
+
+        return NextResponse.json({message: "Subscribed successfully", subscriptionEnds: updatedUser.subscriptionEnds})
+    } catch (error) {
+        console.error("Error updating subscription", error)
+        return NextResponse.json({error: "Internal server error"}, {status: 500})
+    }
+}
+
+export async function GET() {
+    const {userId} = await auth()
+
+    if(!userId) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    }
+
+    try {
+        const user = await prisma.user.findUnique(
+            {
+                where: {id: userId},
+                select: {subscriptionEnds: true, isSubscribed: true}
+            }
+        )
+
+        if(!user) {
+            return NextResponse.json({error: "User not found"}, {status: 404})
+        }
+
+        const now = new Date()
+
+        if(user.subscriptionEnds && user.subscriptionEnds < now) {
+            await prisma.user.update(
+                {
+                    where: {id: userId},
+                    data: {
+                        isSubscribed: false,
+                        subscriptionEnds: null
+                    }
+                }
+            )
+
+            return NextResponse.json({isSubscirbed: false, subscriptionEnds: null})
+        }
+
+        return NextResponse.json({isSubscribed: user.isSubscribed, subscriptionEnds: user.subscriptionEnds})
+    } catch (error) {
+        console.error("Error fetching subscription details", error)
+        return NextResponse.json({error: "Internal server error"}, {status: 500})
+    }
+
+}
