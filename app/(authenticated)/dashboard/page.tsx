18d29d27f179deaf428ac5733111463dc9074b8e
Aayush <aayush130405@gmail.com>
2025-09-23 23:03:17 +0530

    modify dashboard page

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -9,14 +9,29 @@ function Dashboard() {
     const {user} = useUser()
     const [todos, setTodos] = useState<Todo[]>([])
     const [searchTerm, setSearchTerm] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [totalPages, setTotalPages] = useState('')
+    const [currentPage, setCurrentPage] = useState('')
 
     const [debouncedSearchTerm] = useDebounceValue(searchTerm, 300)     //to reduce unnecessary api calls
 
     const fetchTodos = useCallback(async (page: number) => {
         try {
+            setLoading(true)
             const response = await fetch(`/api/todos?page=${page}&search=${debouncedSearchTerm}`)
+
+            if(!response.ok) {
+                throw new Error("Failed to fetch todos")
+            }
+
+            const data = await response.json()
+            setTodos(data.todos)
+            setTotalPages(data.totalPages)
+            setCurrentPage(data.currentPage)
+
+            setLoading(false)
         } catch (error) {
-            
+            setLoading(false)
         }
     }, [debouncedSearchTerm])
 
