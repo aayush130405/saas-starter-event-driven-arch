2d63a3b88fbb87052591901277e078c6aa0da9ff
Aayush <aayush130405@gmail.com>
2025-09-23 23:02:42 +0530

    add dashboard page

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,12 +1,25 @@
 "use client"
 
 import { useUser } from '@clerk/nextjs'
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import { Todo } from '@/app/generated/prisma'
+import { useDebounceValue } from 'usehooks-ts'
 
 function Dashboard() {
     const {user} = useUser()
     const [todos, setTodos] = useState<Todo[]>([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const [debouncedSearchTerm] = useDebounceValue(searchTerm, 300)     //to reduce unnecessary api calls
+
+    const fetchTodos = useCallback(async (page: number) => {
+        try {
+            const response = await fetch(`/api/todos?page=${page}&search=${debouncedSearchTerm}`)
+        } catch (error) {
+            
+        }
+    }, [debouncedSearchTerm])
+
   return (
     <div>Dashboard</div>
   )
